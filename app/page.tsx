352be1f5bd944ec3ce517a4e26278509377985a4
wallyKKKKK <wallylohan12@gmail.com>
2026-05-11 11:48:22 -0300

    atualização da box de criação

--- app/page.tsx
+++ app/page.tsx
@@ -243,6 +243,7 @@ export default function App() {
         </div>
       </div>
 
+
       <main className="max-w-4xl mx-auto p-4">
         {/* DASHBOARD */}
         {activeTab === 'DASHBOARD' ? (
